002b4f5b0cc954aabbb19a943a330d7742e79b5c
SAlexey <shestakov.alexy@gmail.com>
2023-04-16 15:46:24 +0200

    resize main window

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-## a pygame game 
+## a pygame game
 import pygame
 from pygame.locals import *
 from pygame.time import *
 import random
 import math
 
+
 # define a main function
 def main():
-     
     # initialize the pygame module
     pygame.init()
     # load and set the logo
     pygame.display.set_caption("minimal program")
-     
+
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((400,180))
-     
+    screen = pygame.display.set_mode((400, 400))
+
     # define a variable to control the main loop
     running = True
-     
+
     # main loop
     while running:
         # event handling, gets all event from the event queue
@@ -27,10 +27,10 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-     
-     
+
+
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
-if __name__=="__main__":
+if __name__ == "__main__":
     # call the main function
     main()
