cafb72831af0a9df07d0a790c816e4cbc443535e
SAlexey <shestakov.alexy@gmail.com>
2023-05-18 12:13:25 +0200

    chore: add docstrings to properties

--- src/character/base.py
+++ src/character/base.py
@@ -22,20 +22,35 @@ class Character:
 
     @property
     def is_alive(self) -> bool:
+        """
+        Check if the character is alive
+        """
         return self.health > 0
 
     @property
     def melee_attack(self) -> int:
-        return self.strength + self.dexterity
+        """
+        Calculate the melee attack points
+        """
+        return self.strength + self.intelligence
 
     @property
     def ranged_attack(self) -> int:
+        """
+        Calculate the ranged attack points
+        """
         return self.dexterity + self.intelligence
 
     @property
     def special_attack(self) -> int:
+        """
+        Calculate the special attack points
+        """
         return self.strength + self.intelligence + self.dexterity
 
     @property
     def max_experience(self) -> int:
+        """
+        Calculate the max experience points
+        """
         return self.level * 100 + 1
